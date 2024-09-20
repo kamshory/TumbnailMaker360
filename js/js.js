38361d5ade5aa990def71e7bfaabf8bb9beabc16
Kamshory, MT <kamshory@yahoo.com>
2024-09-21 00:07:51 +0700

    First commit

---
+++ js/js.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function(e1) {
+    document.body.addEventListener('paste', (event) => {
+        const item = event.clipboardData.items[0];
+        if (item.type.indexOf('image') === 0) {
+            const blob = item.getAsFile();
+            const reader = new FileReader();
+            reader.onload = function() {
+                const imageUrl = reader.result;
+                document.querySelector('.image').style.backgroundImage = `url('${imageUrl}')`;
+            };
+            reader.readAsDataURL(blob);
+        }
+    });
+
+    document.querySelector('#title').addEventListener('click', function(e2){
+        e2.target.setAttribute('contenteditable', 'true')
+    });
+    document.querySelector('#title').addEventListener('blur', function(e2){
+        e2.target.removeAttribute('contenteditable')
+    });
+    document.querySelector('#logo').addEventListener('click', function(e2){
+        html2canvas(document.querySelector("body")).then(canvas => {
+            const dataURL = canvas.toDataURL("image/jpeg"); 
+            saveAs(dataURL, "thumbnail.jpg");
+        });
+    });
+});
+
